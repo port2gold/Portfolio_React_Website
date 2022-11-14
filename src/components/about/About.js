61f3656f7020fe4262e0defcc37f00cee45faa5c
Abdulkabir Omotoso <aomotoso@fsdhgroup.com>
2022-11-14 22:44:56 +0100

    update

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -41,14 +41,6 @@ const About = () => {
             sem. Massa enim nec dui nunc mattis enim ut. At erat pellentesque
             adipiscing commodo elit at imperdiet dui accumsan. Diam sit amet
             nisl suscipit adipiscing bibendum. Lorem ipsum dolor sit amet
-            consectetur adipiscing elit ut. Nec ultrices dui sapien eget mi
-            proin sed. Pellentesque nec nam aliquam sem et tortor. Viverra
-            tellus in hac habitasse platea dictumst vestibulum rhoncus. Accumsan
-            in nisl nisi scelerisque. Ridiculus mus mauris vitae ultricies leo
-            integer malesuada. Pellentesque habitant morbi tristique senectus et
-            netus. Volutpat est velit egestas dui id ornare arcu odio. Sit amet
-            tellus cras adipiscing enim eu turpis egestas pretium. Semper eget
-            duis at tellus at urna condimentum mattis. Nec tincidunt praesent
           </p>
           <a href="#contact" className="btn btn-primary">
             Let's Talk
